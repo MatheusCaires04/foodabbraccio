b081c75aaee2daf9bacffb6dc82781bb849b7283
MatheusCaires04 <matheus-bini@hotmail.com>
2023-02-03 09:20:11 -0300

    Projeto completo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import logo from "/assets/abbraccio_logo.svg";
 
 const Logo = () => {
   return (
-    <div className="bg-white fixed top-0 md:top-14 left-0 sm:left-[50%] sm:translate-x-[-50%] md:translate-x-0 md:left-0 px-5 sm:px-10 md:pl-12 md:pr-10 py-1 sm:py-7 sm:hover:pt-12 md:hover:pt-7 md:hover:pl-16 group duration-300 z-[1000]">
+    <div className="bg-white fixed top-0 md:top-14 left-0 sm:left-[50%] sm:translate-x-[-50%] md:translate-x-0 md:left-0 px-5 sm:px-10 md:pl-12 md:pr-10 py-1 sm:py-7 sm:hover:pt-12 md:hover:pt-7 md:hover:pl-16 group duration-300 z-[10]">
       <a href="/" className="group-hover:opacity-50 duration-300">
         <img
           src={logo}
